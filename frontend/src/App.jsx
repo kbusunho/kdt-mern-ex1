9c4e51b99f1660882bef39a013060072655e08f8
kbusunho <135941352+kbusunho@users.noreply.github.com>
2025-08-13 11:35:12 +0900

    update

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import PostForm from './components/PostForm'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+      <div>
+        <PostForm />
+    </div>
+  )
+}
+
+export default App
